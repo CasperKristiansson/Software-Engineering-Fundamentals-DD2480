488b09859b68dcfb235986bcdd9d9606317c23af
Martin Lindefors <lindefor@kth.se>
2024-01-30 10:36:50 +0100

    Add test class for cmv.py

---
+++ tests/test_cmv.py
@@ -0,0 +1,60 @@
+import unittest
+import sys
+import os
+
+# Add root folder to current path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+import src.cmv
+
+class TestCMV(unittest.TestCase):
+
+    # Placeholder functions - Most likely need more than one test per function
+
+    def test_construct_vector(self):
+        pass
+
+    def test_condition0(self):
+        pass
+
+    def test_condition1(self):
+        pass
+
+    def test_condition2(self):
+        pass
+
+    def test_condition3(self):
+        pass
+
+    def test_condition4(self):
+        pass
+
+    def test_condition5(self):
+        pass
+
+    def test_condition6(self):
+        pass
+
+    def test_condition7(self):
+        pass
+
+    def test_condition8(self):
+        pass
+
+    def test_condition9(self):
+        pass
+
+    def test_condition10(self):
+        pass
+
+    def test_condition11(self):
+        pass
+
+    def test_condition12(self):
+        pass
+
+    def test_condition13(self):
+        pass
+
+    def test_condition14(self):
+        pass
