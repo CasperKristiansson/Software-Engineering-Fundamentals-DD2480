efc3fb3332807cdb5aefeec5e3d6fe1a2e926e47
Lindefor <83233481+Lindefor@users.noreply.github.com>
2024-01-31 17:33:40 +0100

    Issue/8 lic#6 (#57)
    * feat: add LIC#6
    
    * docs: Added documentation to LIC#6
    
    * add: tests and input files for condition6 method in cmv
    
    closes #8

--- src/cmv.py
+++ src/cmv.py
@@ -1,3 +1,4 @@
+import math
 
 class CMV:
     def __init__(self, d):
@@ -43,7 +44,50 @@ class CMV:
         pass
 
     def condition6(self):
-        pass
+        """
+        Determine whether there exists at least one set of N_PTS consecutive data points
+        where at least one of the points (p_0) is a distance greater than DIST from the line between
+        the first (p_1) and the last (p_2) of the N_PTS. If p_1 and p_2 coincide, then determine whether any
+        of the other N_PTS is a distance greater than DIST from p_1 (the coincident point)
+
+        Parameters:
+        - self: the CMV object
+
+        Returns:
+        - True if any of the described criteria is fulfilled
+        - False otherwise
+        """
+
+        if self.NUMPOINTS < 3:
+            return False
+
+        for i in range(len(self.POINTS)-self.N_PTS+1):
+            n_points_array = self.POINTS[i:i+self.N_PTS]
+            p_1 = n_points_array[0]
+            p_2 = n_points_array[-1]
+            if p_1 != p_2:
+                for point_index in range(1, len(n_points_array)-1):
+                    p_0 = n_points_array[point_index]
+                    x_0, y_0 = p_0[0], p_0[1]
+                    x_1, y_1 = p_1[0], p_1[1]
+                    x_2, y_2 = p_2[0], p_2[1]  
+                    distance_to_line = abs((x_2 - x_1)*(y_1 - y_0) - (x_1 - x_0)*(y_2 - y_1)) \
+                                        / math.sqrt(math.pow(x_2 - x_1, 2) + math.pow(y_2 - y_1, 2))
+
+                    if distance_to_line > self.DIST:                        
+                        return True
+            else:
+                for point_index in range(1, len(n_points_array)-1):
+                    p_0 = n_points_array[point_index]
+                    x_0, y_0 = p_0[0], p_0[1]
+                    x_1, y_1 = p_1[0], p_1[1]
+                    distance_to_point = math.sqrt(math.pow(x_1 - x_0,2) + math.pow(y_1 - y_0, 2))
+                    if distance_to_point > self.DIST:
+                        return True
+        
+        return False
+
+
 
     def condition7(self):
         pass
--- tests/test_cmv.py
+++ tests/test_cmv.py
@@ -22,3 +22,49 @@ class TestCMV(unittest.TestCase):
         """
         d = read_input_to_dict(filepath)
         return CMV(d)
+    
+    def test_condition6(self):
+        """
+        Test the condition6 method of the CMV class.
+
+        This method checks the behavior of the condition6 method under various scenarios:
+        1. An instance where the requirement is fulfilled
+        2. An instance where the requirement is not fulfilled
+        3. An instance where the list is too short
+        4. An instance where the first and last points coincide, where the requirement is still fulfilled
+        5. An instance where the first and last points coincide, but where the requirement is not fulfilled
+        
+        Parameters:
+        - self: The instance of the test class.
+        
+        Returns:
+        - None
+        """
+        filename="condition6/cmv_cond6_accept.in"
+
+        cmv = self.instantiate_object(os.path.dirname(__file__)+"/data/"+filename)
+        self.assertTrue(cmv.condition6())
+
+        filename="condition6/cmv_cond6_reject.in"
+        cmv = self.instantiate_object(os.path.dirname(__file__)+"/data/"+filename)
+        self.assertFalse(cmv.condition6())
+
+        cmv.NUMPOINTS = 2
+        self.assertFalse(cmv.condition6())
+
+        filename="condition6/cmv_cond6_identical_points_accept.in"
+        cmv = self.instantiate_object(os.path.dirname(__file__)+"/data/"+filename)
+        self.assertTrue(cmv.condition6())
+
+        filename="condition6/cmv_cond6_identical_points_reject.in"
+        cmv = self.instantiate_object(os.path.dirname(__file__)+"/data/"+filename)
+        self.assertFalse(cmv.condition6())
+
+
+        
+
+
+
+
+
+
